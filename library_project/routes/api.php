b7279aa5f8c786c92ff7bed22146421d62c78e98
Agnes-milia <ratipotti@gmail.com>
2025-11-07 16:47:36 +0100

    autentikáció rendben

--- library_project/routes/api.php
+++ library_project/routes/api.php
@@ -1,22 +1,33 @@
 <?php
 
-use App\Http\Controllers\LendingController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
 
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/{id}', [UserController::class, 'show']);
-Route::post('/users', [UserController::class, 'store']);
-Route::put('/users/{id}', [UserController::class, 'update']);
-Route::delete('/users/{id}', [UserController::class, 'destroy']);
+//bárki hozzáférhet
+Route::post('/register',[RegisteredUserController::class, 'store']);
+Route::post('/login',[AuthenticatedSessionController::class, 'store']);
 
-Route::get('/lendings', [LendingController::class, 'index']);
-Route::get('/lendings/{user_id}/{copy_id}/{start}', [LendingController::class, 'show']);
-Route::post('/lendings', [LendingController::class, 'store']);
-Route::put('/lendings/{user_id}/{copy_id}/{start}', [LendingController::class, 'update']);
-Route::delete('/lendings/{user_id}/{copy_id}/{start}', [LendingController::class, 'destroy']);
+//autentikált felh-ó
+Route::middleware(['auth:sanctum'])
+->group(function () {
+    Route::get('/users/{id}', [UserController::class, 'show']);
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
+});
+
+//admin
+Route::middleware(['auth:sanctum', Admin::class])
+->group(function () {
+    Route::get('/users', [UserController::class, 'index']);
+});
+
+
+
+
